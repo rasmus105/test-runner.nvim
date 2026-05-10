371f3c1bb7e1d63b47a8a48346ec9d070c3b1a75
Rasmus Kargo <rasmus105@protonmail.com>
2026-05-10 13:07:02 +0200

    test(zig): add script helper for running tests with custom zig build-runner

---
+++ tests/fixtures/zig/run-custom-build-runner.sh
@@ -0,0 +1,11 @@
+SCRIPT_DIR="$(realpath "$(dirname "$0")")"
+
+export TRNVIM_EVENT_DIR="$SCRIPT_DIR/.trnvim-events"
+export TRNVIM_TEST_RUNNER="$SCRIPT_DIR/../../../lua/test-runner/adapters/zig/test_runner.zig"
+
+mkdir -p "$TRNVIM_EVENT_DIR"
+rm -f "$TRNVIM_EVENT_DIR"/*.jsonl
+
+zig build --build-runner \
+        "$SCRIPT_DIR/../../../lua/test-runner/adapters/zig/build_runner.zig" \
+        test
